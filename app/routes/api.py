71c33cbd98d916b4b7cbfb8addab1d6a3027c105
D3C0D1NG <joseph.s.foster@icloud.com>
2024-01-04 18:53:04 -0600

    added login routes w error handling

---
+++ app/routes/api.py
@@ -0,0 +1,33 @@
+import sys
+
+from flask import Blueprint
+from app.models import User
+from app.db import get_db
+from flask import Blueprint, request, jsonify
+
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
+
+    db.add(newUser)
+    db.commit()
+    
+  except:
+      print(sys.exe_info()[0])
+      # insert failed, so rollback and send error to front end
+      db.rollback()
+      return jsonify(message = 'Signup failed'), 500
+
+  return jsonify(id = newUser.id)
