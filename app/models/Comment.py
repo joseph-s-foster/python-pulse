44cfab598986351d02f978487d888e87490261fd
D3C0D1NG <joseph.s.foster@icloud.com>
2024-05-06 18:22:21 -0500

    updated comment date format

--- app/models/Comment.py
+++ app/models/Comment.py
@@ -4,12 +4,17 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 class Comment(Base):
-  __tablename__ = 'comments'
-  id = Column(Integer, primary_key=True)
-  comment_text = Column(String(255), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  post_id = Column(Integer, ForeignKey('posts.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-  user = relationship('User')
+    user = relationship('User')
+    post = relationship('Post')
+
+    @property
+    def formatted_created_at(self):
+        return self.created_at.strftime("%d %b, %Y")  # Format the date as "04 May, 2024"
